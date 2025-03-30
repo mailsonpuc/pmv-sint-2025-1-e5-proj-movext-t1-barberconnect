7b703ad8e38538b1fd18ed43c6689497540e64cf
fredzampier <fredzampier@gmail.com>
2025-03-30 16:46:04 -0300

    modelos
    modelos

---
+++ src/backend/Models/Appointment.cs
@@ -0,0 +1,20 @@
+﻿namespace backend.Models
+{
+    public class Appointment
+    {   
+        public int Id { get; set; }
+
+        public int BarberId { get; set; }
+        
+        public Barber Barber { get; set; }
+
+        public int ClientId { get; set; }
+
+        public Client Client { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+
+        public Review? Review { get; set; }
+    }
+}
