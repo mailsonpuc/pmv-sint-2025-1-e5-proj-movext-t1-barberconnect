7b3cd1a4306093e00f5826279368008a8bc5d994
Lizandra Ruiz <71288985+lizandraRuiz@users.noreply.github.com>
2025-04-20 21:15:11 -0300

    Add files via upload

---
+++ src/backend/BarberConnect.Api/Comunication/AgendamentoRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberConnect.Api.Comunication
+{
+    public class AgendamentoRequest
+    {
+        public string? Status { get; set; }
+        public int IdCliente { get; set; }
+        public int IdServico { get; set; }
+        public int IdHorario { get; set; }
+        public int IdBarbeiro { get; set; }
+    }
+}
---
+++ src/backend/BarberConnect.Api/Comunication/AgendamentoResponse.cs
@@ -0,0 +1,28 @@
+﻿using BarberConnect.Api.Models;
+
+namespace BarberConnect.Api.Comunication
+{
+    public class AgendamentoResponse
+    {
+        public int IdAgendamento { get; set; }
+        public string? Status { get; set; }
+        public bool? LembreteEnviado { get; set; }
+
+        public Cliente Cliente { get; set; }
+        public Servico Servico { get; set; }
+        public HorarioDisponivel HorarioDisponivel { get; set; }
+        public Barbeiro Barbeiro { get; set; }
+
+        public AgendamentoResponse(Agendamento agendamento)
+        {
+            IdAgendamento = agendamento.IdAgendamento;
+            Status = agendamento.Status;
+            LembreteEnviado = agendamento.LembreteEnviado;
+            Cliente = agendamento.Cliente;
+            Servico = agendamento.Servico;
+            HorarioDisponivel = agendamento.HorarioDisponivel;
+            Barbeiro = agendamento.Barbeiro;
+        }
+
+    }
+}
